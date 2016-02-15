c4a4c3feb6a64e2b377123ca1535c639b5353f4b
Yasser Toruno <ytoruno@gmail.com>
2016-02-15 01:09:27 -0500

    gulp: add task to generate pdf from html file

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var less = require('gulp-less');
 var settings = {
   build_dir: './build/',
   less_dir: './less/',
+  margin: '15',
 };
 
 gulp.task('clean', function() {
@@ -28,3 +29,14 @@ gulp.task('watch', function() {
     settings.less_dir + '**/*.less'
   ], ['build']);
 });
+gulp.task('pdf', shell.task(
+  [
+    'wkhtmltopdf',
+    '--margin-left ' + settings.margin,
+    '--margin-right ' + settings.margin,
+    '--zoom 1.0',
+    '--viewport-size 1280x1024',
+    settings.build_dir + 'resume_printable.html',
+    'resume.pdf'
+  ].join(' ')
+));
